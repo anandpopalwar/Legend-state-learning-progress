063f7dfcf12f074f6a90f2155605f50f3ada64fa
emai-ap <ap@easemyai.com>
2025-02-22 13:34:50 +0530

    second commit adding modal ,added cards which are checkable , and onClick add the prize to variable, but the problem is total prize is not rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,69 @@
-import { For, Show, useObservable } from "@legendapp/state/react";
+import {
+  For,
+  Memo,
+  observer,
+  Show,
+  useComputed,
+  useObservable,
+  useObserve,
+  useSelector,
+} from "@legendapp/state/react";
 import "./App.scss";
 import backend_data from "./device_data.json";
-import { useRef } from "react";
-// console.log(device_data);
+import { useEffect, useRef } from "react";
+import {
+  Button,
+  CameraIcon,
+  DocIcon,
+  LocationIcon,
+  SolidButton,
+} from "./Button/Button";
+import { Reactive } from "@legendapp/state/react";
+import { observable } from "@legendapp/state";
+const { isModalOpen, devices_data } = observable({
+  isModalOpen: false,
+  devices_data: [...backend_data.devices],
+  // totalPrize: 0,
+  // isBtnDisabled: false,
+});
 
-function App() {
+const closeFunction = () => {
+  isModalOpen.set(false);
+};
+
+const App = () => {
   const renderCount = ++useRef(0).current;
-  const { isModalOpen, devices_data } = useObservable({
-    isModalOpen: false,
-    devices_data: [...backend_data.devices],
+
+  const { totalPrize } = useObservable({
+    totalPrize: 0,
   });
+
+  useEffect(() => {
+    console.log("useeffect");
+
+    window.addEventListener("keydown", (e) => {
+      e.code === "Escape" && isModalOpen.get() === true && closeFunction();
+    });
+    return () => {
+      window.removeEventListener("keydown");
+    };
+  }, []);
+
+  // useObserve(totalPrize, (e) => {
+  //   document.title = `${e.get()}`;
+  // });
+
   return (
-    <div className="main_app_container">
-      {renderCount}
+    <div
+      className="main_app_container"
+      onClick={() => {
+        // isBtnDisabled.toggle();
+      }}
+    >
+      {/* {console.log(totalPrize.get(), "totalPrize")} */}
+      <div>{totalPrize.get()}</div>
+      {/* {console.log(totalPrize.get(), "totalPrize")} */}
+      <div className="">{renderCount}</div>
       <button
         onClick={() => {
           isModalOpen.set(true);
@@ -20,34 +71,233 @@ function App() {
       >
         add Devices
       </button>
-      <Show if={isModalOpen}>
-        <div
-          className="modal_wrapper"
-          onClick={() => {
-            isModalOpen.set(false);
-          }}
-        >
-          <div
-            className="modal_container"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            <For each={devices_data}>
-              {(item) => <Card Data={item} key={item.name.get()} />}
-            </For>
-          </div>
-        </div>
-      </Show>
+      {/* <Button2 isBtnDisabled={isBtnDisabled} key={"id"} /> */}
+
+      <Show if={isModalOpen}>{() => <Modal totalPrize={totalPrize} />}</Show>
     </div>
   );
-}
+};
+
+const Modal = ({ totalPrize }) => {
+  const renderCount = ++useRef(0).current;
+
+  // const totalValue = useComputed(() => totalPrize.get() + "");
+  return (
+    <Reactive.div className="modal_wrapper">
+      {console.log(totalPrize.get(), "totalValue")}
+      <Reactive.div className="render_count">{renderCount}</Reactive.div>
+      <Reactive.div
+        className="modal_container"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <Reactive.div className="modal_header">
+          <Reactive.div className="modal_header_title">
+            Add To Devices
+          </Reactive.div>
+          <Reactive.div
+            className="close_button"
+            onClick={(e) => {
+              e.stopPropagation();
+              closeFunction();
+            }}
+          >
+            X
+          </Reactive.div>
+        </Reactive.div>
+        <Reactive.div className="modal_body">
+          {/* <Card Data={devices_data[0]} key={devices_data[0].name.get()} /> */}
+          <For each={devices_data}>
+            {(item) => (
+              <Card Data={item} key={item.id.get()} totalPrize={totalPrize} />
+            )}
+          </For>
+        </Reactive.div>
+        <Reactive.div className="modal_footer">
+          <Reactive.div className="total">
+            {/* SubTotal () : <Memo>{() => totalPrize.get()} </Memo> */}
+            SubTotal () : {totalPrize.get()}
+          </Reactive.div>
+          <Reactive.div className="modal_footer_btns_container">
+            <SolidButton
+              name={"Dismiss"}
+              type={"slatewhite"}
+              width="150px"
+              size="md"
+              id={"dismiss"}
+              key={"dismiss"}
+              borderRadius={"2"}
+              style={{ margin: "0px", padding: ".2vh 1vw" }}
+            />
+            <SolidButton
+              disabled={totalPrize.get() === 0}
+              name={"Buy Now"}
+              width="150px"
+              style={{ margin: "0px", padding: ".2vh 1vw" }}
+              borderRadius={"2"}
+              type={"solidblue"}
+              size="md"
+              id={"buy_now"}
+              key={"buy_now"}
+            />
+          </Reactive.div>
+        </Reactive.div>
+      </Reactive.div>
+    </Reactive.div>
+  );
+};
 
 export default App;
 
-const Card = ({ Data }) => {
+const Card = ({ Data, totalPrize }) => {
   const renderCount = ++useRef(0).current;
 
-  console.log(Data.get());
-  return <div>{renderCount}Card</div>;
+  // console.log(Data.get());
+  // console.log(Data.name.get(), Data.isChecked.get(), "--------");
+  return (
+    <Reactive.div
+      className="card_container"
+      onClick={() => {
+        Data.isChecked.set((prev) => !prev);
+        console.log(Data.name.get(), Data.isChecked.get(), ">>>>>>>>>>>>");
+
+        Data.isChecked.get()
+          ? totalPrize.set((prev) => prev + Data.price.get())
+          : totalPrize.set((prev) => prev - Data.price.get());
+      }}
+    >
+      <Reactive.div className="left_side">
+        <Reactive.div className="render_count">{renderCount}</Reactive.div>
+        <Reactive.div className="device_name">{Data.name.get()}</Reactive.div>
+        <Reactive.div className="badges_contianer">
+          <Button
+            name={Data.location.get()}
+            color={"#216E4E"}
+            bgShade
+            fontWeight={"600"}
+            borderRadius={2}
+            LeftIcon={LocationIcon}
+            width="100px"
+            style={{ margin: "0px" }}
+          />
+          <Button
+            name={"Camera : " + Data.camera_count.get()}
+            color={"#004B91"}
+            bgShade
+            fontWeight={"600"}
+            borderRadius={2}
+            LeftIcon={CameraIcon}
+            width="100px"
+            style={{ margin: "0px" }}
+          />
+          <Button
+            name={"No. of Licenses : " + Data.licenses.get()}
+            color={"#733FB5"}
+            bgShade
+            fontWeight={"600"}
+            borderRadius={2}
+            LeftIcon={DocIcon}
+            width="100px"
+            style={{ margin: "0px" }}
+          />
+        </Reactive.div>
+        <div className="card_footer">
+          <div className="card_footer_title">License Details</div>
+          <div className="card_footer_items">
+            {/* { (Data.license_details).map((item) => (
+              <Button
+                name={item + ":" + Data.license_details[item].get()}
+                monoChrome
+                border
+                fontWeight={"600"}
+                borderWeight={"2px"}
+                borderColor={"#e2e4e9"}
+                borderRadius="1"
+                width="100px"
+                LeftIcon={DocIcon}
+                style={{ margin: "0px" }}
+                key={Data.id.get() + item}
+                readOnly
+              />
+            ))} */}
+            <For each={Data.license_details}>
+              {(item) => (
+                <Button
+                  name={item.type.get() + ":" + item.value.get()}
+                  monoChrome
+                  border
+                  fontWeight={"600"}
+                  borderWeight={"2px"}
+                  borderColor={"#e2e4e9"}
+                  borderRadius="1"
+                  width="100px"
+                  LeftIcon={DocIcon}
+                  style={{ margin: "0px", padding: "0px 2px" }}
+                  key={item.id.get()}
+                  readOnly
+                />
+              )}
+            </For>
+          </div>
+        </div>
+      </Reactive.div>
+      <Reactive.div className="right_side">
+        {/* <Reactive.input
+          $type="checkbox"
+          $value={Data.isChecked}
+          $onChange={(e) => {
+            Data.isChecked.set((prev) => !prev);
+            // Data.isChecked.get();
+            console.log(Data.isChecked.get(), ">>>>>>>>>>>>");
+          }}
+        /> */}
+        <Show if={Data?.isChecked} else={() => <div>⬜</div>}>
+          {() => <div>✅</div>}
+        </Show>
+        {/* <Memo>
+          {() => (
+            <Button
+              name={Data?.isChecked?.get() ? "✅" : "⬜"}
+              border
+              width="10px"
+              style={{ margin: "0px" }}
+              readOnly
+            />
+          )}
+        </Memo> */}
+      </Reactive.div>
+    </Reactive.div>
+  );
+};
+
+const Button2 = ({ isBtnDisabled }) => {
+  // const { classList } = useObservable({ classList: "button" });
+  const renderCount = ++useRef(0).current;
+  console.log(isBtnDisabled.get(), "isBtnDisabled");
+  // if (isBtnDisabled.get()) {
+  //   // classList.set((p) => p + " disabled ");
+  //   console.log("loggggggggggggggg");
+  // }
+  // return <button className={classList.get()}>Anand{renderCount}</button>;
+
+  // useEffect(() => {
+  //   console.log("XXXXXXXXXX");
+  // }, [isBtnDisabled.get()]);
+  const isSelected = useComputed(() =>
+    isBtnDisabled.get() ? "button" : "button disabled"
+  );
+
+  console.log(isSelected.get(), "isSelected");
+  return (
+    <Reactive.button
+      $className={() => isSelected.get()}
+      // $className={() => classList.get()}
+      // $className={() =>
+      //   !isBtnDisabled.get() ? "button " : " button disabled "
+      // }
+    >
+      Anand{renderCount}
+    </Reactive.button>
+  );
 };
---
+++ src/Button/Button.jsx
@@ -0,0 +1,347 @@
+import { observer, Reactive } from "@legendapp/state/react";
+import "./Button.scss";
+import { useRef } from "react";
+// import { DocIcon, InfoIcon } from "./App3";
+const ButtonsContainer = () => {
+  return (
+    <div className="App3">
+      ButtonsContainer
+      <p
+        style={{
+          color: "red",
+          textAlign: "center",
+        }}
+      >
+        solid buttons
+      </p>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "10px",
+        }}
+      >
+        <InfoIcon />
+        <SolidButton
+          name={"button"}
+          width="100px"
+          borderRadius={"2"}
+          type={"solidblue"}
+          id="primary_btn"
+          LeftIcon={InfoIcon}
+          size="lg"
+        />
+        <SolidButton
+          name={"button"}
+          type={"slatewhite"}
+          LeftIcon={DocIcon}
+          width="100px"
+          size="md"
+        />
+        <SolidButton
+          name={"button"}
+          type={"warningred"}
+          LeftIcon={DocIcon}
+          width="100px"
+        />
+        <SolidButton
+          name={"button"}
+          type={"solidgreen"}
+          LeftIcon={DocIcon}
+          width="100px"
+        />
+        <SolidButton
+          name={"button"}
+          type={"solidblack"}
+          LeftIcon={DocIcon}
+          width="100px"
+        />
+        <Button
+          name={"button"}
+          extraClassName="neutral"
+          LeftIcon={DocIcon}
+          width="100px"
+        />
+      </div>
+      <p
+        style={{
+          color: "red",
+          textAlign: "center",
+        }}
+      >
+        monochrome buttons
+      </p>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <Button
+          name={"button"}
+          monoChrome
+          border
+          borderWeight={"2px"}
+          borderColor={"#e2e4e9"}
+          borderRadius="1"
+          width="100px"
+        />
+        <Button
+          name={"button"}
+          monoChrome
+          border
+          borderWeight={"2px"}
+          borderColor={"#e2e4e9"}
+          borderRadius="1"
+          LeftIcon={DocIcon}
+          width="100px"
+        />
+        <Button name={"button"} monoChrome link width="100px" />
+      </div>
+      <p
+        style={{
+          color: "red",
+          textAlign: "center",
+        }}
+      >
+        bg shade button buttons
+      </p>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <Button
+          name={"button"}
+          color={"#FF0000"}
+          bgShade
+          fontWeight={"600"}
+          borderRadius={2}
+          LeftIcon={InfoIcon}
+          width="100px"
+        />
+        <Button
+          name={"button"}
+          borderRadius={1}
+          color={"#216E4E"}
+          bgShade
+          fontWeight={"600"}
+          width="100px"
+        />
+        <Button
+          name={"button"}
+          LeftIcon={DocIcon}
+          color={"#004B91"}
+          bgShade
+          fontWeight={"600"}
+          width="100px"
+        />
+        <Button
+          name={"button"}
+          color={"#733FB5"}
+          bgShade
+          fontWeight={"600"}
+          width="100px"
+        />
+      </div>
+    </div>
+  );
+};
+
+export const SolidButton = observer(
+  ({
+    name,
+    LeftIcon,
+    RightIcon,
+    size,
+    extraClassName,
+    type = "solidblue",
+    borderRadius,
+    disabled,
+    id,
+    style,
+    width,
+  }) => {
+    // console.log(props, "props");
+    let type2 =
+      "solidblue" | "slatewhite" | "warningred" | "solidgreen" | "solidblack";
+    // let props = { name, LeftIcon, RightIcon, extraClassName, type, disabled };
+
+    return (
+      <Button
+        extraClassName={[type, extraClassName]}
+        {...{
+          name,
+          LeftIcon,
+          RightIcon,
+          disabled,
+          borderRadius,
+          size,
+          id,
+          style,
+          width,
+        }}
+      />
+    );
+  }
+);
+
+export const Button = observer(
+  ({
+    name,
+    LeftIcon,
+    RightIcon,
+    extraClassName = "",
+    bgShade = false,
+    monoChrome,
+    color,
+    borderRadius = false,
+    border = false,
+    borderColor = false,
+    borderWeight = false,
+    disabled = false,
+    readOnly = false,
+    onClick,
+    width,
+    height,
+    size = "sm",
+    fontWeight,
+    style,
+    link,
+    id,
+    verticle,
+  }) => {
+    //   size = "sm" | "md" | "lg";
+
+    const renderCount = ++useRef(0).current;
+
+    const styles = {
+      width: !width && "100px",
+      height,
+      cursor: disabled && "not-allowed",
+      opacity: disabled && 0.8,
+      borderRadius: !borderRadius && "2px",
+      textDecoration: link && "underline",
+      color,
+      fontWeight,
+      backgroundColor:
+        color && bgShade && `color-mix(in oklab, ${color} 20%,  white)`,
+      ...style,
+    };
+    //   console.log(LeftIcon, "LeftIcon");
+    //   console.log(bgShade, color, `color.scale(${color},  $lightness: 90%)`);
+    if (border) {
+      styles["border"] =
+        borderWeight + " solid " + (borderColor ? borderColor : "transparent");
+    }
+    if (monoChrome) {
+      styles["backgroundColor"] = "white";
+      styles["color"] = "#20232D";
+    }
+    if (borderRadius) {
+      styles["borderRadius"] =
+        borderRadius == 1 ? "5px" : borderRadius == 2 && "99px";
+    }
+
+    if (size) {
+      styles["fontSize"] =
+        size === "sm" ? "12px" : size === "md" ? "18px" : "30px";
+
+      if (!width) {
+        styles["width"] =
+          size === "sm" ? "100px" : size === "md" ? "120px" : "150px";
+      }
+
+      styles["height"] =
+        size === "sm" ? "25px" : size === "md" ? "45px" : "70px";
+    }
+
+    let extraClassNameString =
+      extraClassName && Array.isArray(extraClassName)
+        ? extraClassName.join(" ")
+        : extraClassName;
+
+    return (
+      <Reactive.button
+        className={"button " + extraClassNameString}
+        onClick={!readOnly ? onClick : null}
+        disabled={disabled}
+        style={{ ...styles }}
+        id={id}
+      >
+        {renderCount}
+
+        {LeftIcon && (
+          <LeftIcon fill={color} width={styles["fontSize"]} height={"100%"} />
+        )}
+        {name}
+        {RightIcon && (
+          <RightIcon fill={color} width={styles["fontSize"]} height={"100%"} />
+        )}
+      </Reactive.button>
+    );
+  }
+);
+
+export const InfoIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={props?.width ?? 16}
+    height={props?.height ?? 16}
+    viewBox={`0 0 16 16`}
+    fill="#20232D"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM9 4C9 4.55228 8.55228 5 8 5C7.44772 5 7 4.55228 7 4C7 3.44772 7.44772 3 8 3C8.55228 3 9 3.44772 9 4ZM7 7C6.44772 7 6 7.44772 6 8C6 8.55229 6.44772 9 7 9V12C7 12.5523 7.44772 13 8 13H9C9.55228 13 10 12.5523 10 12C10 11.4477 9.55228 11 9 11V8C9 7.44772 8.55228 7 8 7H7Z"
+    />
+  </svg>
+);
+
+export const DocIcon = (props) => (
+  <svg
+    width={props?.width ?? 16}
+    height={props?.height ?? 16}
+    viewBox="0 0 24 24"
+    fill="#000"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M17 5H7a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1M7 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3z"
+    />
+    <path d="M8 7h8v2H8zm0 4h8v2H8zm0 4h5v2H8z" />
+  </svg>
+);
+
+export const LocationIcon = (props) => (
+  <svg
+    width={props?.width ?? 16}
+    height={props?.height ?? 16}
+    viewBox="0 0 0.96 0.96"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#20232D"
+    {...props}
+  >
+    <path d="M.48.03a.33.33 0 0 0-.33.33c0 .337.3.555.313.564a.03.03 0 0 0 .035 0C.51.915.81.697.81.36A.33.33 0 0 0 .48.03m0 .48A.15.15 0 1 1 .63.36a.15.15 0 0 1-.15.15" />
+  </svg>
+);
+export const CameraIcon = (props) => (
+  <svg
+    width={props?.width ?? 16}
+    height={props?.height ?? 16}
+    viewBox="0 0 0.72 0.72"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#20232D"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M0.227 0.13A0.09 0.09 0 0 1 0.302 0.09h0.116a0.09 0.09 0 0 1 0.075 0.04l0.024 0.037A0.03 0.03 0 0 0 0.542 0.18H0.57a0.09 0.09 0 0 1 0.09 0.09v0.27a0.09 0.09 0 0 1 -0.09 0.09H0.15A0.09 0.09 0 0 1 0.06 0.54V0.27A0.09 0.09 0 0 1 0.15 0.18h0.028A0.03 0.03 0 0 0 0.203 0.167zM0.3 0.39a0.06 0.06 0 1 1 0.12 0 0.06 0.06 0 0 1 -0.12 0M0.36 0.27a0.12 0.12 0 1 0 0 0.24 0.12 0.12 0 0 0 0 -0.24"
+      clipRule="evenodd"
+    />
+  </svg>
+);
